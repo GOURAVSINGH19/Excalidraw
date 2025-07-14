4445cc92adb6acf42af36b38d256eca3ceb8ce28
GOURAVSINGH19 <gouravsingh4495@gmail.com>
2025-07-14 19:33:02 +0530

    init-prisma-optimization

--- packages/db/src/prisma.ts
+++ packages/db/src/prisma.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const PrismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof PrismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? PrismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
